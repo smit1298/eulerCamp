f27da5d3c2a2ab60e7eb88de41bf1ef3a86c8ff0
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-02-21 00:51:37 +0100

    modified evenAndOdd
    restructured the code

--- evenAndOdd.js
+++ evenAndOdd.js
@@ -2,8 +2,8 @@ showNumbers(10);
 
 function showNumbers(limit) {
   for (let i = 0; i <= limit; i++) {
-    let determine = i % 2 === 0;
-    if (determine === "true") console.log(i, "EVEN");
-    else console.log(i, "ODD");
+    let determine = (i % 2 === 0);
+    const message = (determine) ? 'EVEN' : 'ODD';
+    console.log(i, message);   
   }
 }
